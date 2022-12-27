7aed006af5e43136e222017cae45b29d8004d0d1
adrian <adrianperezm.00@gmail.com>
2022-12-26 23:53:44 -0700

    fix: HashRouter to display router in gh-pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route, Link as PageLink } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route, Link as PageLink } from 'react-router-dom'
 import { useComposeProviders } from 'hooks'
 
 import Layout from 'layout'
