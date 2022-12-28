6a9df4e9cea943fca2dfe1b8c9df22bd9289d47f
adrian <adrianperezm.00@gmail.com>
2022-12-28 01:07:43 -0700

    feat: TextField, add style properties

--- src/components/inputs/TextField/index.js
+++ src/components/inputs/TextField/index.js
@@ -1,10 +1,11 @@
-import React, { forwardRef, memo, useCallback, useEffect, useImperativeHandle, useRef, useState } from 'react'
+import React, { forwardRef, memo, useCallback, useEffect, useImperativeHandle, useMemo, useRef, useState } from 'react'
 import reactFastCompare from 'react-fast-compare'
 
-const TextField = forwardRef( function TextField (props, ref) {
+const TextField = forwardRef(function TextField(props, ref) {
     /*------------------------------------PROPS--------------------------------*/
-    let { className, label, value = '', onChange, placeholder, ...rest } = props
-    
+    let { className, label, value = '', onChange, placeholder,
+        padding, margin, w100, h100, maxWidth, style, ...rest } = props
+
     /*------------------------------------STATE---------------------------------*/
     const [focused, setFocused] = useState(false)
 
@@ -27,27 +28,36 @@ const TextField = forwardRef( function TextField (props, ref) {
     }), [textFieldRef, value])
 
     /*------------------------------------EFFECT-------------------------------*/
-    useEffect(() => () => {}, [])
+    useEffect(() => () => { }, [])
 
     /*------------------------------------RENDER-------------------------------*/
     className = className ? `${className} text_field_container` : 'text_field_container'
-    
+
+    style = useMemo(() => ({
+        padding,
+        margin,
+        width: w100 && '100%',
+        height: h100 && '100%',
+        maxWidth,
+        ...style
+    }), [padding, margin, w100, h100, maxWidth])
+
     !label && (className += ' no_label')
-    if(value !== '' || focused) className += ' label_up'
+    if (value !== '' || focused) className += ' label_up'
     focused && (className += ' focused')
-    
+
     return (
-        <div className={className}>
+        <div className={className} style={style}>
             {label && <label className='text_field__label'>{label}</label>}
             <div className='text_field'>
-                <input 
+                <input
                     ref={textFieldRef}
-                    className='text_field__input' 
-                    onChange={handleChange} 
-                    onFocus={handleFocus} 
+                    className='text_field__input'
+                    onChange={handleChange}
+                    onFocus={handleFocus}
                     onBlur={handleFocus}
-                    placeholder={(focused || !label) ? placeholder:''}
-                    {...{value, ...rest}}
+                    placeholder={(focused || !label) ? placeholder : ''}
+                    {...{ value, ...rest }}
                 />
                 <fieldset className='text_field__fieldset'>
                     <legend className='text_field__legend'>{label}</legend>
