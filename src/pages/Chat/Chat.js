27f1e4e243e9974432281ab091bfe551636357b3
adrian <adrianperezm.00@gmail.com>
2022-12-28 01:09:04 -0700

    feat: Add page Chat

--- src/pages/Chat/Chat.js
+++ src/pages/Chat/Chat.js
@@ -1,22 +1,50 @@
-import { Grid } from 'components'
-import React, { useCallback, useState } from 'react'
+import { Grid, TextField } from 'components'
+import React, { useEffect, useState } from 'react'
 import './chat_styles.scss'
+import useSocket from 'context/hooks/useSocket'
 
 const Chat = () => {
 
-    const [message, setMessage] = useState('');
+    const { socket } = useSocket()
 
-    const handleChangeTextArea = useCallback(e => {
-        setMessage(message => e.target.value)
+    const [user, setUser] = useState('')
+    const [message, setMessage] = useState('')
+    const [messages, setMessages] = useState([])
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        socket.emit('message', { user, content: message })
+
+        const newMessage = { user: 'Me', content: message }
+
+        setMessages([newMessage, ...messages])
+        setMessage('')
+    }
+
+    useEffect(() => {
+        const messageHandler = message => { console.log(message); setMessages(messages => [message, ...messages]) }
+        socket.on('message', messageHandler)
+        return () => {
+            socket.off('message', messageHandler)
+        }
     }, [])
-    
+
     return (
         <Grid className='chat' gap='0.5em'>
-            <Grid className='chat__textarea' w100 gap='0.5em' maxWidth='90%'>
-                <label htmlFor='message'>Message</label>
-                <textarea id="message" value={message} onChange={handleChangeTextArea} rows={2}/>
+            <TextField maxWidth='20em' label='User' value={user} onChange={setUser} />
+            <form onSubmit={handleSubmit}>
+                <Grid w100 className='chat__textarea' gap='0.5em' maxWidth='90%' columns='1fr auto' itemsY='end'>
+                    <TextField label='Message' value={message} onChange={setMessage} />
+                    <button className='chat_button'>Send</button>
+                </Grid>
+            </form>
+
+            <Grid>
+                {messages.map((message, i) =>
+                    <p key={i} className='message'>{message.user}: {message.content}</p>
+                )}
             </Grid>
-            <button className='chat_button'>Send message</button>
+
         </Grid>
     )
 }
