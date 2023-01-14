9844df1ce585c883b5ba58e98fc0e8be8e931725
adrian <adrianperezm.00@gmail.com>
2023-01-13 17:28:15 -0700

    home icon

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,12 @@
 import { Grid } from 'components'
 import React from 'react'
+import { AiFillHome } from 'react-icons/ai'
 
 const Home = () => {
     return (
-        <Grid contentX="center" contentY="center">
-            <h1>Home Page!</h1>
+        <Grid contentX="center" contentY="center" itemsX="center">
+            <AiFillHome size={30}/>
+            <h1>Home Page</h1>
         </Grid>
     )
 }
