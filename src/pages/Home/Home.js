e94a485b2fd60b40dbbae8570417c291b2418aed
adrian <adrianperezm.00@gmail.com>
2023-01-15 00:42:52 -0700

    print if cookie token is empty

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,15 @@
 import { Grid } from 'components'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { AiFillHome } from 'react-icons/ai'
 
 const Home = () => {
+    useEffect(() => {
+        if (global.getCookieToken() == '')
+            console.log('cookie token is empty')
+    }, [])
     return (
         <Grid contentX="center" contentY="center" itemsX="center">
-            <AiFillHome size={30}/>
-            
+            <AiFillHome size={30} />
             <h1>Home Page</h1>
         </Grid>
     )
