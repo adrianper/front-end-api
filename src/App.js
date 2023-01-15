dc4f0a4cd91ceeee5116f135c9661c0914e73dcc
adrian <adrianperezm.00@gmail.com>
2023-01-14 23:51:47 -0700

    add homepage and hashrouter for gh pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import { useComposeProviders } from 'hooks'
 
 import { Provider as ReduxProvider } from 'react-redux'
